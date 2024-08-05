0990da60342e698595a5c7aecb64e6c0174037b3
Darius Teng <dariusyteng@gmail.com>
2024-08-04 21:44:09 -0400

    Added webpack from main

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,10 @@
 const path = require('path');
 module.exports = {
   entry: './src/index.js', // Entry point of call your mother application
-
+  output: {
+    filename: 'bundle.js', // Output bundle file name
+    path: path.resolve(__dirname, 'dist'), // Output directory
+  },
   module: {
     rules: [
       {
@@ -13,16 +16,11 @@ module.exports = {
       },
     ],
   },
-  output: {
-    filename: 'bundle.js', // Output bundle file name
-    path: path.resolve(__dirname, 'dist'), // Output directory
-  },
   resolve: {
     extensions: ['.js', '.jsx'],
   },
   devServer: {
-    // contentBase: path.join(__dirname, "public"), // Serve files from this directory
-    static: path.resolve(__dirname, 'dist'),
+    contentBase: path.join(__dirname, 'public'), // Serve files from this directory
     port: 3000, // Port for the development server
     open: true, // Open the default web browser when the server starts
   },
